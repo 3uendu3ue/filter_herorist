1c1971f3491fe7c4bf5dada7c03554e83d6e37b3
mohamad_herorist <107700829+3uendu3ue@users.noreply.github.com>
2022-07-20 14:09:13 +0430

    Add files via upload

---
+++ filtering_herorist.py
@@ -0,0 +1,178 @@
+#import
+import os
+import time
+import sys
+import datetime
+import requests
+
+
+# code colors
+red='\033[31m'  #قرمز
+green='\033[32m' #سبز
+blue='\033[36m' #ابی
+pink='\033[35m'  #بنفش
+rang='\033[34m' #ابی تیره
+yellow='/033[93m'  #زرد
+
+#dat
+dat = (datetime.datetime.today())
+yellow,green="\033[93m","\033[92m"
+
+
+#print_mohamad.herorist
+print(f"{yellow} ")
+banner = (f"""  MOHAMAD HERORIST GOD FILTERING""")
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+
+#clear
+os.system("clear")
+print()
+      
+            
+#baner
+print(f"{red} ")
+banner = (f"""      
+
+                                               
+               ..:::::::::..
+           ..:::aad8888888baa:::..
+        .::::d:?88888888888?::8b::::.
+      .:::d8888:?88888888??a888888b:::.
+    .:::d8888888a8888888aa8888888888b:::.
+   ::::dP::::::::88888888888::::::::Yb::::
+  ::::dP:::::::::Y888888888P:::::::::Yb::::    
+ ::::d8:::::::::::Y8888888P:::::::::::8b::::
+.::::88::::::::::::Y88888P::::::::::::88::::.   
+:::::Y8baaaaaaaaaa88P:T:Y88aaaaaaaaaad8P:::::
+:::::::Y88888888888P::|::Y88888888888P:::::::    
+::::::::::::::::888:::|:::888::::::::::::::::
+`:::::::::::::::8888888888888b::::::::::::::'  
+ :::::::::::::::88888888888888::::::::::::::
+  :::::::::::::d88888888888888:::::::::::::
+   ::::::::::::88::88::88:::88::::::::::::
+    `::::::::::88::88::88:::88::::::::::'
+      `::::::::88::88::88:::88::::::::'
+        `::::::88::88::88:::88::::::'
+           ``:::::::::::::::::::''
+                ``:::::::::::""")
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.009)
+
+
+        
+print("")        
+print("")        
+print("")        
+print("")        
+        
+        
+print(f"{rang} ")
+banner = (f"""{yellow}《{dat}
+ {rang}                 
+ _ __ ___   ___ | |__   __ _ _ __   __ _  __| |
+| '_ ` _ \ / _ \| '_ \ / _` | '_ \ / _` |/ _` |
+| | | | | | (_) | | | | (_| | | | | (_| | (_| |
+|_| |_| |_|\___/|_| |_|\__,_|_| |_|\__,_|\__,_|
+
+                                  
+| |__   ___ _ __ ___  _ __(_)___| |_
+| '_ \ / _ \ '__/ _ \| '__| / __| __|
+| | | |  __/ | | (_) | |  | \__ \ |_
+|_| |_|\___|_|  \___/|_|  |_|___/\__|
+   """)
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.009)
+
+        
+        
+            
+#start
+print("")
+print("")
+print("")
+print("")
+time.sleep(1.7)
+print ("🖤")
+time.sleep(0.5)
+print ("🖤🖤")
+time.sleep(0.4)
+print ("🖤🖤🖤")
+time.sleep(0.3)
+print ("🖤🖤🖤🖤")
+time.sleep(0.2)
+print ("🖤🖤🖤🖤🖤")
+time.sleep(0.1)
+print ("🖤🖤🖤🖤🖤🖤")
+time.sleep(0.5)
+print ("🖤🖤🖤🖤🖤🖤🖤")
+time.sleep(0.1)
+print ("🖤🖤🖤🖤🖤🖤🖤🖤")
+time.sleep(0.5)
+print("🖤🖤🖤🖤🖤🖤🖤🖤🖤")
+time.sleep(0.1)
+
+print("")
+print("")
+
+
+#input
+up = input(""" 
+[cood filter Account rubika (1) ]
+[cood filter channel rubika (2) ]
+[cood filtr group rubika    (3) ]         
+
+Which do you want>>>""")
+
+
+#!
+print (blue)
+print("\rscanning..",end="",flush=False)
+time.sleep(1)
+print("\rscanning....",end="",flush=False)
+time.sleep(1)
+print("\rscanning.....",end="",flush=False)
+time.sleep(1)
+print("\rscanning......",end="",flush=False)
+time.sleep(1)
+print("\rscanning.......",end="",flush=False)
+time.sleep(0.5)
+print("\rscanning........",end="",flush=False)
+time.sleep(0.5)
+print("\rscanning.........",end="",flush=False)
+time.sleep(0.5)
+print("\rscanning..........",end="",flush=False)
+time.sleep(1)
+print("\r scanned          ",end="",flush=False)
+
+
+print("")
+print("")
+print("")
+
+
+#clear
+os.system("clear")
+print()
+#cod filteri acoonut
+if up == "1":
+         print ("""  \033[41m(https://.lnpker.dlit.http//(reaprot.(81.23.40.7)_senotor.pro(6.0.3.6/d///f//i///y.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.:bit.ly/3ild93L1.2.4.0.3.4.6.0.3.6.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.5.5.1.2.4.0.3.4)https://bit.ly/3ild93L(+bit.ly/9ploftakhttps+×CFh)\33[1;0m  """)
+   
+         
+                     
+#cod filteri channel     
+if up == "2":
+         print ("""  \033[41m(7.8.9.2.d/f///y//6065///g.k.4.6.7.8.2.3.5.6.7.3.2.9.3.:bit.ly/3ild93L.9.1.5.3.6.2.9.6/.7.8.9.2.4.6.7.8.2.3.5.6.7.3.2.9.3.9.1.5.3.6.2.9.6/)\33[1;0m   """)
+         
+ 
+           
+#cod filteri group                 
+if up == "3":
+        print("""" \033[41m(4.0.1.4/f.i.d/gl/4.5.3//5.2.5.7.2.6.8.4.2.4.7.9.3.1.5.2.7.9.8.:bit.ly/3ild93L.0.1.3.4.0.1.4.4.5.3.5.2.5.7.2.6.8.4.2.4.7.9.3.1.5.2.7.9.8.0.1.3)\33[1;0m  """)   
